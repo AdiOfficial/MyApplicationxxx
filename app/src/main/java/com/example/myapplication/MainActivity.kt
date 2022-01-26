2f2ba28c1727a15b0b6c4b234d8a1499db17c553
Adi Jaya <adijaya4538@gmail.com>
2022-01-26 15:23:40 +0700

    aku ngubah MainActivity.kt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AlertDialog.Builder(this)
+            .setMessage("KOnfirmasi")
+            .setPositiveButton("OK"){d,i,->}
+            .create().show()
     }
 }
